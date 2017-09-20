58469bf7a8e43597eb84b613c2f4dc7f8df14877
Brandon Him <brandonhim@live.com>
2017-09-20 16:41:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
-'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+export default class PMRObserver {
+    constructor(observable, calendarService) {
+        if (!observable) {
+            throw new Error('Observable is expected, in order to subscribe');
+        }
 
-	opts = opts || {};
+        if (!calendarService) {
+            throw new Error('CalendarService is expected');
+        }
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
-};
+        this.observable = observable;
+        this.calendar = calendarService;
+    }
+
+    handler = (event) => {
+        if (this.shouldProcess(event)) {
+            this.processEvent(event);
+        }
+    }
+
+    shouldProcess(event) {
+        if (!event.summary) {
+            return false;
+        }
+
+        const summaryContainsWebex = event.summary.match(/@webex/i);
+        if (summaryContainsWebex) {
+            return true;
+        }
+    }
+
+    processEvent = (event) => {
+        const descriptionExist = event.description;
+        if (descriptionExist) {
+            if (event.description.indexOf('WebEx Details') > 0) {
+                // do nothing if it already has webex details
+                return;
+            }
+        }
+
+        const existingDescription = (event.description) ? event.description : '';
+        const updateInfo = {
+            description: this.buildDescription(existingDescription, CMR, event.userId),
+            colorId: 9
+        };
+
+        const updatedEvent = Object.assign({}, event, updateInfo);
+        this.calendar.updateEvent({
+            calendarId: event.calendarId,
+            eventId: event.id
+        }, updatedEvent)
+        .then(() => debug("Sucessfully update event."))
+        .catch(debug);
+    }
+
+    buildDescription(existingDescription, cmrSite, userId) {
+        return `${existingDescription}
+==== WebEx Details: Do Not Touch ====
+http://${cmrSite}.webex.com/meet/${userId}`;
+    }
+
+    init() {
+        observable.subscribe(this.handler);
+    }
+}
